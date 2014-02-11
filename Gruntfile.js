297163fb2e66d7a80abc2290d6747ae2af5b8afb
Ivan Storck <ivanoats@gmail.com>
2014-02-11 08:04:46 -0800

    adding jshint, still needs options for mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,20 @@ module.exports = function(grunt) {
       },
 
       all: { src: ['test/**/*.js'] }
+    },
+    jshint: {
+      all: ['Gruntfile.js','test/**/*.js']
     }
   });
 
   // For this to work, you need to have run `npm install grunt-simple-mocha`
   grunt.loadNpmTasks('grunt-simple-mocha');
 
+  // JSHint
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   // Add a default task. This is optional, of course :)
   grunt.registerTask('default', 'simplemocha');
+
 
 };
