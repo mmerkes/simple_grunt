578e74891314405e818f84e1cdd69cda24fda0a7
Empty Arsenal <matt.merkes@gmail.com>
2014-02-12 09:44:45 -0800

    Finished simple grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,10 @@
 /*global module:false*/
 module.exports = function(grunt) {
+  "use strict";
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     simplemocha: {
       options: {
         timeout: 3000,
@@ -11,13 +13,27 @@ module.exports = function(grunt) {
       },
 
       all: { src: ['test/**/*.js'] }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        globals: {
+          require: true,
+          describe: true,
+          it: true
+        }
+      },
+      files: ['Gruntfile.js', 'test/**/*.js']
     }
   });
 
   // For this to work, you need to have run `npm install grunt-simple-mocha`
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Add a default task. This is optional, of course :)
-  grunt.registerTask('default', 'simplemocha');
+  grunt.registerTask('default', ['simplemocha', 'jshint']);
 
 };
--- test/array_indexOf_spec.js
+++ test/array_indexOf_spec.js
@@ -1,7 +1,6 @@
-'use strict';
-
 var assert = require('assert');
 describe('Array', function(){
+  'use strict';
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(5));
